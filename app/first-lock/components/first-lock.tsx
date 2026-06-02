f05cf5bf61af7fe537eba916fd327e2119b310e0
PatrickCookSAIT <patrick.cook@edu.sait.ca>
2026-06-02 13:48:14 -0600

    first lock complete

--- app/first-lock/components/first-lock.tsx
+++ app/first-lock/components/first-lock.tsx
@@ -1,4 +1,5 @@
 'use client'
+import { useRouter } from 'next/dist/client/components/navigation'
 import React, {useState} from 'react'
 
 const FirstLock = () => {
@@ -6,6 +7,11 @@ const FirstLock = () => {
     const [countTwo, setCountTwo] = useState(4)
     const [countThree, setCountThree] = useState(5)
     const [countFour, setCountFour] = useState(3)
+    const router= useRouter();
+    const handleClick = () => {
+        if(countOne === 1 && countTwo === 2 && countThree === 6 && countFour === 4)
+        router.push("/the-walls");
+    };
 
     const handleUpOne = () => {
         if(countOne === 9)
@@ -24,7 +30,7 @@ const FirstLock = () => {
         }      
     }
     const handleUpTwo = () => {
-        if(countOne === 9)
+        if(countTwo === 9)
         {
             setCountTwo(0)
         } else {
@@ -32,67 +38,96 @@ const FirstLock = () => {
         }      
     }
      const handleDownTwo = () => {
-        if(countOne === 0)
+        if(countTwo === 0)
         {
             setCountTwo(9)
         } else {
-            setCountTwo(countOne - 1)
+            setCountTwo(countTwo - 1)
         }      
     }
     const handleUpThree = () => {
-        if(countOne === 9)
+        if(countThree === 9)
         {
             setCountThree(0)
         } else {
-            setCountThree(countOne + 1)
+            setCountThree(countThree + 1)
         }      
     }
      const handleDownThree = () => {
-        if(countOne === 0)
+        if(countThree === 0)
         {
             setCountThree(9)
         } else {
-            setCountThree(countOne - 1)
+            setCountThree(countThree - 1)
         }      
     }
     const handleUpFour = () => {
-        if(countOne === 9)
+        if(countFour === 9)
         {
             setCountFour(0)
         } else {
-            setCountFour(countOne + 1)
+            setCountFour(countFour + 1)
         }      
     }
      const handleDownFour = () => {
-        if(countOne === 0)
+        if(countFour === 0)
         {
             setCountFour(9)
         } else {
-            setCountFour(countOne - 1)
+            setCountFour(countFour - 1)
         }      
     }
   return (
-    <div className="flex flex-row">
-      <section className="flex flew-col">
-        <button onClick={handleUpOne} className="bg-red-500 m-1">+</button>
-        <span className="bg-red-500">{countOne}</span>
-        <button onClick={handleDownOne} className="bg-red-500 m-1">-</button>
-      </section>
-      <section className="flex flew-co">
-        <button onClick={handleUpTwo}  className="bg-blue-500 m-1">+</button>
-        <span className="bg-blue-500">{countTwo}</span>
-        <button onClick={handleDownTwo} className="bg-blue-500 m-1">-</button>
-      </section>
-      <section className="flex flew-co">
-        <button onClick={handleUpThree} className="bg-green-500 m-1">+</button>
-        <span className="bg-green-500">{countThree}</span>
-        <button onClick={handleDownThree} className="bg-green-500 m-1">-</button>
-      </section>
-      <section className="flex flew-col">
-        <button onClick={handleUpFour} className="bg-yellow-500 m-1">+</button>
-        <span className="bg-yellow-500">{countFour}</span>
-        <button onClick={handleDownFour} className="bg-yellow-500 m-1">-</button>
-      </section>
+    <div className="flex flex-col justify-center items-center">
+        <section className="flex flex-col w-300">
+            <p className="font-bold mb-5">By the lock you find a note:</p>
+            <p className="text-sm text-gray-500 bg-gray-200 flex flex-col">
+                <p>The week has ended in a haze, you've gained a lot in 7 days</p>
+                <p>Today you have four times more as you had the day before. </p>
+                <p>When it began, you had a span measuring 20 more than your two hands.</p>
+                <p>From that day to the next, you gained more by double X</p>
+                <p>X is an amount one greater than, the sum of 23 plus 44 minus 10</p>
+                <p>And X you have once again, as the amount you had 2 days after it began,</p>
+                <p>From that day, two more days go and pass, then on this day you've got 5 times more than the first, banked and clasped</p>
+                <p>To fill the forth day and the fifth, take the second and add the sixth</p>
+                <p>Now that you have all the sums, add them back one by one</p>
+                <p>Thats the number you have won, place it in the lock and see it spun</p>
+            </p>
+        </section>
+        <section className="flex flex-row">
+            <section className="flex flex-col m-1">
+                <button onClick={handleUpOne} className="bg-red-500 m-1 w-20 h-10">+</button>
+                <span className="bg-red-500 w-20 m-1 h-20 flex items-center justify-center">{countOne}</span>
+                <button onClick={handleDownOne} className="bg-red-500 m-1 w-20 h-10">-</button>
+            </section>
+            <section className="flex flex-col m-1">
+                <button onClick={handleUpTwo}  className="bg-blue-500 w-20 m-1 h-10">+</button>
+                <span className="bg-blue-500 w-20 m-1 h-20 flex items-center justify-center">{countTwo}</span>
+                <button onClick={handleDownTwo} className="bg-blue-500 w-20 m-1 h-10">-</button>
+            </section>
+            <section className="flex flex-col m-1">
+                <button onClick={handleUpThree} className="bg-green-500 w-20 m-1 h-10">+</button>
+                <span className="bg-green-500 w-20 m-1 h-20 flex items-center justify-center">{countThree}</span>
+                <button onClick={handleDownThree} className="bg-green-500 w-20 m-1 h-10">-</button>
+            </section>
+            <section className="flex flex-col m-1">
+                <button onClick={handleUpFour} className="bg-yellow-500 w-20 m-1 h-10">+</button>
+                <span className="bg-yellow-500 w-20 m-1 h-20 flex items-center justify-center">{countFour}</span>
+                <button onClick={handleDownFour} className="bg-yellow-500 w-20 m-1 h-10">-</button>
+            </section>
+        </section>
+        <button
+        onClick={handleClick}
+        className="group w-24 h-12 bg-black text-white rounded-full flex items-center justify-center"
+      >
+        {/* Normal text */}
+        <p className="group-hover:hidden">UNLOCK</p>
+
+        {/* Hover text */}
+        <p className="hidden group-hover:block text-white">
+          💀
+        </p>
+      </button>
     </div>
   )
 }
--- app/the-walls/components/wall-select.tsx
+++ app/the-walls/components/wall-select.tsx
@@ -1,4 +1,6 @@
-import React from 'react'
+'use client'
+import { useRouter } from 'next/dist/client/components/navigation'
+import React, {useState} from 'react'
 
 const WallSelect = () => {
     const router= useRouter();
@@ -15,55 +17,66 @@ const WallSelect = () => {
         router.push("/open-your-eyes");
     };
   return (
-    <div>
-       <button
-        onClick={handleClickLeft}
-        className="group w-24 h-12 bg-black text-white rounded-full flex items-center justify-center"
-      >
-        {/* Normal text */}
-        <p className="group-hover:hidden">GO LEFT</p>
+    <div className="flex flex-col">
+        <section>
+            <p>
+                You enter a room draped in bright white light. In front of you lays a box with a combination lock, type of lock you'd use in high school with a 3 number combination.
+                Surrounding the box are three distinct walls. On your left, a wall that is bathed in red light from a fixture overhead. The wall on your right has WIP. And in front of you is the wall with a door. On the door are stange symbols ordered in a grid.
+            </p>
+        </section>
+        <section className="flex items-center justify-center">
+            <button
+            onClick={handleClickForward}
+            className="group w-24 h-12 bg-black text-white rounded-full flex items-center justify-center"
+        >
+            {/* Normal text */}
+            <p className="group-hover:hidden">GO FORWARD</p>
 
-        {/* Hover text */}
-        <p className="hidden group-hover:block text-white">
-          💀
-        </p>
-      </button>
-       <button
-        onClick={handleClickForward}
-        className="group w-24 h-12 bg-black text-white rounded-full flex items-center justify-center"
-      >
-        {/* Normal text */}
-        <p className="group-hover:hidden">GO FORWARD</p>
+            {/* Hover text */}
+            <p className="hidden group-hover:block text-white">
+            💀
+            </p>
+            </button>
+        </section>
+        <section className="flex flex-row items-center justify-center mt-10">
+            <button
+                onClick={handleClickLeft}
+                className="group w-24 h-12 bg-black text-white rounded-full flex items-center justify-center"
+            >
+                {/* Normal text */}
+                <p className="group-hover:hidden">GO LEFT</p>
 
-        {/* Hover text */}
-        <p className="hidden group-hover:block text-white">
-          💀
-        </p>
-      </button>
-       <button
-        onClick={handleClickRight}
-        className="group w-24 h-12 bg-black text-white rounded-full flex items-center justify-center"
-      >
-        {/* Normal text */}
-        <p className="group-hover:hidden">GO RIGHT</p>
+                {/* Hover text */}
+                <p className="hidden group-hover:block text-white">
+                💀
+                </p>
+            </button>
+            
+            <button
+                onClick={handleClickRight}
+                className="group w-24 h-12 bg-black text-white rounded-full flex items-center justify-center mx-5"
+            >
+                {/* Normal text */}
+                <p className="group-hover:hidden">VIEW THE BOX</p>
 
-        {/* Hover text */}
-        <p className="hidden group-hover:block text-white">
-          💀
-        </p>
-      </button>
-       <button
-        onClick={handleClickBox}
-        className="group w-24 h-12 bg-black text-white rounded-full flex items-center justify-center"
-      >
-        {/* Normal text */}
-        <p className="group-hover:hidden">VIEW THE BOX</p>
+                {/* Hover text */}
+                <p className="hidden group-hover:block text-white">
+                💀
+                </p>
+            </button>
+            <button
+                onClick={handleClickBox}
+                className="group w-24 h-12 bg-black text-white rounded-full flex items-center justify-center"
+            >
+                {/* Normal text */}
+                <p className="group-hover:hidden">GO RIGHT</p>
 
-        {/* Hover text */}
-        <p className="hidden group-hover:block text-white">
-          💀
-        </p>
-      </button>
+                {/* Hover text */}
+                <p className="hidden group-hover:block text-white">
+                💀
+                </p>
+            </button>
+        </section>
     </div>
   )
 }
