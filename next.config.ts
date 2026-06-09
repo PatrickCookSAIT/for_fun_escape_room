b68855249e1700a419632fd175f8c0848e05916d
PatrickCookSAIT <patrick.cook@edu.sait.ca>
2026-06-09 11:05:30 -0600

    Update next.config.ts for environment-specific asset prefix
    Refactor next.config.ts to support different asset prefixes based on the development phase.

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
-};
-
-export default nextConfig;
+// @ts-check
+import { PHASE_DEVELOPMENT_SERVER } from 'next/constants'
+ 
+export default (phase) => {
+  const isDev = phase === PHASE_DEVELOPMENT_SERVER
+  /**
+   * @type {import('next').NextConfig}
+   */
+  const nextConfig = {
+    assetPrefix: isDev ? undefined : 'https://cdn.mydomain.com',
+  }
+  return nextConfig
+}
